c591b57b8a1f5487bef7cf4e29fd4dc61eaf45be
Barinder Singh <barindersinghdhanoa@gmail.com>
2026-08-16 13:07:18 +0530

    implement security middleware

--- classroom_project/classroom-backend/src/index.ts
+++ classroom_project/classroom-backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import subjectsRouter from "./routes/subjects";
 import cors from "cors";
+import securityMiddleware from "./middleware/security";
 
 const app = express();
 const PORT = 8000;
@@ -16,6 +17,9 @@ app.use(cors({
 }))
 
 app.use(express.json());
+
+app.use(securityMiddleware);
+
 app.use('/api/subjects', subjectsRouter)
 
 app.get("/", (_req, res) => {
