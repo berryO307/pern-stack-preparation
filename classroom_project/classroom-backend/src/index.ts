5b50fcf60ee8995d2e8f2467abd8a808833f1ad0
Barinder Singh <barindersinghdhanoa@gmail.com>
2026-08-18 03:24:26 +0530

    implement fetch teachers and create classes

--- classroom_project/classroom-backend/src/index.ts
+++ classroom_project/classroom-backend/src/index.ts
@@ -3,6 +3,8 @@ AgentAPI.config()
 
 import express from "express";
 import subjectsRouter from "./routes/subjects.js";
+import usersRouter from "./routes/users.js";
+import classesRouter from "./routes/classes.js";
 import cors from "cors";
 import securityMiddleware from "./middleware/security.js";
 import {toNodeHandler} from "better-auth/node";
@@ -28,6 +30,8 @@ app.use(express.json());
 app.use(securityMiddleware);
 
 app.use('/api/subjects', subjectsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/classes', classesRouter)
 
 app.get("/", (_req, res) => {
   res.send("Classroom backend is up and running!");
