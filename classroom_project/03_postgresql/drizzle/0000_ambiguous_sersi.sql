9d739ef7c87fd4e397acf97e863d8bf29e60baaf
Barinder Singh <149930708+berryO307@users.noreply.github.com>
2026-08-20 23:38:56 +0530

    Delete classroom_project/03_postgresql directory

--- classroom_project/03_postgresql/drizzle/0000_ambiguous_sersi.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "cars" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"make" varchar(100) NOT NULL,
-	"model" varchar(100) NOT NULL,
-	"year" integer NOT NULL,
-	"price" integer NOT NULL,
-	"created_at" timestamp DEFAULT now()
-);
